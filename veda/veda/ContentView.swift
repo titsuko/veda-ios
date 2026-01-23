4cb561a671408ef6e395dc8d9183eba70dd0c4bb
Evgeniy Petrukovich <evgeniy.petrukovich@icloud.com>
2026-01-23 17:09:24 +0300

    Initial commit for veda iOS app
    Add Xcode project files, asset catalogs, and initial SwiftUI app structure with ContentView and vedaApp entry point.

---
+++ veda/veda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  veda
+//
+//  Created by Евгений Петрукович on 1/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
